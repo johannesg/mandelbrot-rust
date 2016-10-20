f3c5aadc360bfce97326567fdd73ebfd19e239e8
Kristoffer Teuber <krite@krite.se>
2016-10-20 12:56:28 +0200

    Single Producer Multiple Consumer introduced. Using only crates.io as import, and no deprecated APIs. Working with rust nightly (1.14.0) and cargo (0.13.0).

--- src/main.rs
+++ src/main.rs
@@ -1,152 +1,124 @@
-#![feature(old_io)]
-#![feature(std_misc)]
-
 extern crate sdl2;
 extern crate num;
+#[macro_use] extern crate chan;
 
-use sdl2::video::{WindowPos, Window, OPENGL};
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::Rect;
 use sdl2::event::{Event};
-use sdl2::surface::{Surface};
-use std::sync::mpsc::{channel, Sender, Receiver};
-#[allow(deprecated)]
-use std::old_io::timer::Timer;
-use std::time::Duration;
+use chan::{Sender, Receiver, WaitGroup};
 use std::thread;
-use num::complex::Complex64;
+use std::io::Write;
 use num::traits::Zero;
+use num::complex::Complex64;
 
-static WIDTH: i32 = 800;
-static HEIGHT: i32 = 600;
-static MAX_ITERATIONS: i32 = 100;
-// static THREADS: i32 = 4;
+static WIDTH: u32 = 400;
+static HEIGHT: u32 = 300;
+static MAX_ITERATIONS: u32 = 20;
+static NUM_THREADS: u32 = 8;
 
-fn main()
-{
-    // start sdl2
-    let ctx = match sdl2::init(sdl2::INIT_VIDEO) {
-        Ok(ctx)     => ctx,
-        Err(err)    => panic!("Failed to start SDL2: {}", err)
-    };
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
 
-    // create a window
-    let window = match Window::new("mandel", WindowPos::PosCentered, WindowPos::PosCentered, WIDTH, HEIGHT, OPENGL) {
-        Ok(window)  => window,
-        Err(err)    => panic!("Failed to create window: {}", err)
-    };
+    let window = video_subsystem.window("Mandelbrot using rust + sdl2", WIDTH, HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
 
-    let screen = match window.get_surface() {
-        Ok(screen)  => screen,
-        Err(err)        => panic!("Failed to create screen: {}", err)
-    };
+    let mut renderer = window.renderer().build().unwrap();
 
-    let buffer_len: usize = WIDTH as usize * HEIGHT as usize * 4 as usize;
+    let buffer_len: usize = WIDTH as usize * HEIGHT as usize * 3 as usize;
     let mut buffer = vec!(0u8; buffer_len);
 
-    let surface = match Surface::from_data(buffer.as_mut_slice(), WIDTH, HEIGHT, 32, WIDTH * 4, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000) {
-        Ok(surface) => surface,
-        Err(err)    => panic!("Failed to create surface: {}", err)
-    };
-
-    let mut timer = Timer::new().unwrap();
-    let periodic = timer.periodic(Duration::milliseconds(50));
-
-    let (work_tx, work_rx) = channel::<(i32,i32)>();
-    let (result_tx, result_rx) = channel::<(i32,i32,i32)>();
+    let mut texture = renderer.create_texture_streaming(PixelFormatEnum::RGB24, WIDTH, HEIGHT).unwrap();
+    let mut events = sdl_context.event_pump().unwrap();
 
     let top_left = Complex64::new(-2f64, 1f64);
     let bottom_right = Complex64::new(1f64, -1f64);
 
-    // no support for multiple consumers???
-    // for thread_id in 0..THREADS {
-    spawn_worker(work_rx, result_tx, top_left, bottom_right);
-    // }
-    
-    generate_work(work_tx);
-    
-    let mut events = ctx.event_pump();
-    // loop until we receive a QuitEvent
-    'event : loop {
-        select! {
-            res = result_rx.recv() => {
-                if res.is_err() {
-                    continue;
-                }
+    let (work_tx, work_rx) = chan::async();
+    let (result_tx, result_rx) = chan::async();
+    let tick = chan::tick_ms(500);
+    let wg = WaitGroup::new();
 
-                let (x, y, n) = res.unwrap();
-                let c = ((n * 255) / MAX_ITERATIONS) as u8;
-                let offset = (WIDTH * y * 4 + x * 4) as usize;
-                buffer[offset] = c;
-                buffer[offset+1] = c;
-                buffer[offset+2] = c;
-                buffer[offset+3] = 255;
-            },
-            _ = periodic.recv() => {
-                // poll_event return the most recent event or NoEvent if nothing has happened
+    for _ in 0..NUM_THREADS {
+        wg.add(1);
+        let wg = wg.clone();
+        spawn_worker(work_rx.clone(), result_tx.clone(), wg, top_left, bottom_right);
+    }
+
+    generate_work(&work_tx);
+
+    'event: loop {
+        chan_select! {
+            default => {},
+            tick.recv() => {
                 for event in events.poll_iter() {
                     match event {
-                        Event::Quit{..} => break 'event,
-                        _               => continue
+                        Event::Quit { .. } => break 'event,
+                        _ => continue
                     }
                 }
-                screen.blit(&surface, None, None);
-                window.update_surface();
+
+                texture.with_lock(None, |mut screen_buffer: &mut [u8], pitch: usize| {
+                    screen_buffer.write(&mut buffer).unwrap();
+                }).unwrap();
+                renderer.clear();
+                renderer.copy(&texture, None, Some(Rect::new(0, 0, WIDTH, HEIGHT)));
+                renderer.present();
+            },
+            result_rx.recv() -> message => {
+                let (x, y, n) = message.unwrap();
+                let c = ((n * 255) / MAX_ITERATIONS) as u8;
+                let offset = (WIDTH * y * 3 + x * 3) as usize;
+                buffer[offset] = c;
+                buffer[offset + 1] = c;
+                buffer[offset + 2] = c;
             }
         }
     }
 }
 
-fn generate_work(work: Sender<(i32,i32)>) {
+fn generate_work(work_tx: &Sender<(u32, u32)>) {
     for y in 0..HEIGHT {
-       for x in 0..WIDTH {
-           let _ = work.send((x,y));
-       } 
+        for x in 0..WIDTH {
+            let _ = work_tx.send((x, y));
+        }
     }
 }
 
-fn spawn_worker(
-    work: Receiver<(i32,i32)>, 
-    result : Sender<(i32,i32,i32)>,
-    top_left : Complex64,
-    bottom_right : Complex64
-    ) {
-
+fn spawn_worker(work_rx: Receiver<(u32, u32)>, result_tx: Sender<(u32, u32, u32)>,
+                wg: WaitGroup, top_left: Complex64, bottom_right: Complex64) {
     let delta = bottom_right - top_left;
-
-    let scale = Complex64 { 
-        re: delta.re / WIDTH as f64, 
+    let scale = Complex64 {
+        re: delta.re / WIDTH as f64,
         im: delta.im / HEIGHT as f64
     };
 
     thread::spawn(move || {
-        loop {
-            let (x,y) = match work.recv() {
-                Ok(res) => res,
-                Err(_) => { println!("Complete"); break },
-            };
-
+        for (x, y) in work_rx {
             let (xf, yf) = (x as f64, y as f64);
             let dot = Complex64::new(
                 scale.re * xf + top_left.re,
                 scale.im * yf + top_left.im
-                );
+            );
             let n = iterate(dot);
-
-            let _ = result.send((x, y, n));
+            let _ = result_tx.send((x, y, n));
         }
+        wg.done();
     });
 }
 
-fn iterate(c : Complex64) -> i32 {
-	let mut z = Complex64::zero();
-	let mut znorm = 0f64;
-	let mut n = 0i32;
+fn iterate(c: Complex64) -> u32 {
+    let mut z = Complex64::zero();
+    let mut znorm = 0f64;
+    let mut n = 0u32;
 
-	while n < MAX_ITERATIONS && znorm < 4.0 {
-		z = z*z + c;
-		znorm = z.norm_sqr();
+    while n < MAX_ITERATIONS && znorm < 4.0 {
+        z = z * z + c;
+        znorm = z.norm_sqr();
         n += 1;
-	}
-
+    }
     n
 }
-
